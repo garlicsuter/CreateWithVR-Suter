cd2ccbe788fe68defe29717a110067fac4865217
garlicsuter <markasuter@gmail.com>
2021-07-30 11:49:49 -0400

    Section 1 of CwVR complete
    room setup
    teleport to rugs
    custom picture frame

---
+++ Assets/MarksNotes.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarksNotes : MonoBehaviour
+{
+/*
+ Create with VR course Progress:
+ 1 VR Basics Complete
+    - Custom picture frame: New Material, base map is jpeg. Material "tiling" x and y scrub until it fits.
+
+
+*/
+}
