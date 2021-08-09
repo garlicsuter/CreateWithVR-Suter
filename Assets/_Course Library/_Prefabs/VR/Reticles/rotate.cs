d3cb3d88ad60eaf02557865e12d9840188157e63
garlicsuter <markasuter@gmail.com>
2021-08-09 16:26:27 -0400

    rotating reticle works!

--- Assets/_Course Library/_Prefabs/VR/Reticles/rotate.cs
+++ Assets/_Course Library/_Prefabs/VR/Reticles/rotate.cs
@@ -5,38 +5,12 @@ using UnityEngine;
 public class rotate : MonoBehaviour
 {
     
-    //public Texture myTexture1;
-    //public Texture myTexture2;
-    //public Texture myTexture3;
-    //public Texture myTexture4;
-    //private Renderer myRenderer;
-    //public Mesh theReticle;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        //transform.Rotate(0, 222, 50);
-        //myRenderer = GetComponent<Renderer>();
-
-        //Call SetColor using the shader property name "_Color" and setting the color to red
-        //myRenderer.material.SetColor("_Color", Color.red);
-    }
+   
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(1, 3, 1, Space.Self);
-        //theReticle.transform.Rotate(2, 4*Time.deltaTime, 8,Space.World);
-        
-        //myRenderer.material.mainTexture = myTexture1;
-        //myRenderer.material.mainTexture = myTexture2;
-        //myRenderer.material.mainTexture = myTexture3;
-        //myRenderer.material.mainTexture = myTexture4;
-        //myRenderer.material.SetColor("_Color", Color.blue);
+        transform.Rotate(0, 3, 0, Space.World);
     }
 
-    public void DoRotate()
-    {
-        transform.Rotate(1, 3, 1, Space.Self);
-    }
 }
