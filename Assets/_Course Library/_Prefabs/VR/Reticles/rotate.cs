65eded08d7ac3045399ea55ffb6f0692d143d2b0
garlicsuter <markasuter@gmail.com>
2021-08-09 11:58:36 -0400

    more spinning reticle fails

--- Assets/_Course Library/_Prefabs/VR/Reticles/rotate.cs
+++ Assets/_Course Library/_Prefabs/VR/Reticles/rotate.cs
@@ -4,25 +4,39 @@ using UnityEngine;
 
 public class rotate : MonoBehaviour
 {
-    //public GameObject ret;
-    public Texture myTexture;
-    private Renderer myRenderer;
+    
+    //public Texture myTexture1;
+    //public Texture myTexture2;
+    //public Texture myTexture3;
+    //public Texture myTexture4;
+    //private Renderer myRenderer;
+    //public Mesh theReticle;
 
     // Start is called before the first frame update
     void Start()
     {
         //transform.Rotate(0, 222, 50);
-        myRenderer = GetComponent<Renderer>();
+        //myRenderer = GetComponent<Renderer>();
 
         //Call SetColor using the shader property name "_Color" and setting the color to red
-        myRenderer.material.SetColor("_Color", Color.red);
+        //myRenderer.material.SetColor("_Color", Color.red);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //transform.Rotate(1, 3, 1, Space.Self);
-        transform.Rotate(0, 4, 0,Space.World);
-        myRenderer.material.mainTexture = myTexture;
+        transform.Rotate(1, 3, 1, Space.Self);
+        //theReticle.transform.Rotate(2, 4*Time.deltaTime, 8,Space.World);
+        
+        //myRenderer.material.mainTexture = myTexture1;
+        //myRenderer.material.mainTexture = myTexture2;
+        //myRenderer.material.mainTexture = myTexture3;
+        //myRenderer.material.mainTexture = myTexture4;
+        //myRenderer.material.SetColor("_Color", Color.blue);
+    }
+
+    public void DoRotate()
+    {
+        transform.Rotate(1, 3, 1, Space.Self);
     }
 }
