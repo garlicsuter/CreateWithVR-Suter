0b60942ce0e608c8c351937bff108afc2842b0da
garlicsuter <markasuter@gmail.com>
2021-08-16 19:18:27 -0400

    magnifying glass working

---
+++ Assets/_Course Library/Scripts/MirrorMatChange.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorMatChange : MonoBehaviour
+{
+    public Material Clear;
+    public Material Mirror;
+    private Material theMat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theMat = GetComponent<Renderer>().material;
+    }
+
+    public void SetMatClear()
+    {
+        GetComponent<Renderer>().material = Clear;
+    }
+
+    public void SetMatMirror()
+    {
+        GetComponent<Renderer>().material = Mirror;
+    }
+}
