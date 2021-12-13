69b97076639c92ff8bdea90483768daf1464b5a2
Mark Suter <msuter@live.elida.k12.oh.us>
2021-12-13 10:16:45 -0500

    New StudentEdits.cs script
    For students to add their answers, push, pull, merge, branch

---
+++ Assets/_Course Library/Scripts/StudentEdits.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StudentEdits : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // This is Mr. Suter.
+        // Each student should write a comment with their name, followed by the 99 bottles script
+        // Write a short script that will log to the console a message that counts down from
+        // "99 bottles of ketchup on the wall." to 1 BOTTLE (singular, not plural!)
+        // BELOW IS A WRONG WAY TO DO IT.
+
+        Debug.Log("Mr. Suter sang,");
+        Debug.Log("99 bottles of ketchup on the wall");
+        Debug.Log("98 bottles of ketchup on the wall");
+        Debug.Log("97 bottles of ketchup on the wall");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
