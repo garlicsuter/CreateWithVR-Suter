0b50306ac5451ea93a8c7adfd2136195575f24b9
Makkusu-Tieu <23tieuk@live.elida.k12.oh.us>
2021-12-15 09:59:03 -0500

    Bottles of Ketchup on the Wall Made
    I attached the script to an object that I made and named "DELETE ME LATER I HAVE STUDENT SCRIPT" to test if the script worked properly. The object is below the room at ( 0. -2. 0)

--- Assets/_Course Library/Scripts/StudentEdits.cs
+++ Assets/_Course Library/Scripts/StudentEdits.cs
@@ -4,19 +4,17 @@ using UnityEngine;
 
 public class StudentEdits : MonoBehaviour
 {
+    //My Name is Max Tieu ^-^
+    int lowBottle = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-        // This is Mr. Suter.
-        // Each student should write a comment with their name, followed by the 99 bottles script
-        // Write a short script that will log to the console a message that counts down from
-        // "99 bottles of ketchup on the wall." to 1 BOTTLE (singular, not plural!)
-        // BELOW IS A WRONG WAY TO DO IT.
-
-        Debug.Log("Mr. Suter sang,");
-        Debug.Log("99 bottles of ketchup on the wall");
-        Debug.Log("98 bottles of ketchup on the wall");
-        Debug.Log("97 bottles of ketchup on the wall");
+        for (int numBottle = 99; numBottle>lowBottle; numBottle--)
+        {
+            Debug.Log(numBottle + " Bottles of Ketchup on the Wall");
+        }
+        Debug.Log("1 Bottle of Ketchup on the Wall");
     }
 
     // Update is called once per frame
