544af2166062e6fd5dd9fa351747a70005b2a461
B29-Beta <24schud@live.elida.k12.oh.us>
2021-12-15 10:02:28 -0500

    Please work
    Description stuff here

--- Assets/_Course Library/Scripts/StudentEdits.cs
+++ Assets/_Course Library/Scripts/StudentEdits.cs
@@ -7,21 +7,18 @@ public class StudentEdits : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // This is Mr. Suter.
-        // Each student should write a comment with their name, followed by the 99 bottles script
-        // Write a short script that will log to the console a message that counts down from
-        // "99 bottles of ketchup on the wall." to 1 BOTTLE (singular, not plural!)
-        // BELOW IS A WRONG WAY TO DO IT.
-
-        Debug.Log("Mr. Suter sang,");
-        Debug.Log("99 bottles of ketchup on the wall");
-        Debug.Log("98 bottles of ketchup on the wall");
-        Debug.Log("97 bottles of ketchup on the wall");
+        //This is Drew, [insert comment here]
+        for (int i = 99; i > lowestBottle; i--)
+        {
+            Debug.Log(i + "bottles of ketchup on the wall");
+        }
+        Debug.Log("1 bottle of ketchup on the wall");
     }
+}
 
     // Update is called once per frame
     void Update()
     {
         
     }
-}
+
